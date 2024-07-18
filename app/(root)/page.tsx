f7136b86b167fc42e4e951bfeb2f924bb9a31ee7
Ashish Kumar Saini <ashishsaini.ak@gmail.com>
2024-07-18 21:59:29 +0530

    added sidebar mobile nav (#3)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,9 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
       <p>Home</p>
-
-      <UserButton afterSwitchSessionUrl="/" />
     </div>
   );
 };
