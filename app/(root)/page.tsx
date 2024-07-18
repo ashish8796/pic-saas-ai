91f281231f4f86799256711da47d17fe5ef98cd3
ashish8796 <ashishsaini.ak@gmail.com>
2024-07-18 17:15:21 +0530

    added sign-in and sign-up route using clerk and auth routes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,14 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <p>Home</p>
+
+      <UserButton afterSwitchSessionUrl="/" />
+    </div>
+  );
 };
 
 export default Home;
