33135a7082b82141854ecac7b86c9d89867f09c3
ashish8796 <ashishsaini.ak@gmail.com>
2024-07-18 22:38:12 +0530

    added db connection and image schema

---
+++ lib/db/models/image.model.ts
@@ -0,0 +1,57 @@
+import { model, models, Schema } from "mongoose";
+
+const imageSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  transformationType: {
+    type: String,
+    required: true,
+  },
+  publicId: {
+    type: String,
+    required: true,
+  },
+  secureUlr: {
+    type: String,
+    required: true,
+  },
+  width: {
+    type: Number,
+  },
+  height: {
+    type: Number,
+  },
+  config: {
+    type: Object,
+  },
+  transformationUrl: {
+    type: URL,
+  },
+  aspectRatio: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+  prompt: {
+    type: String,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Image = models.Image || model("Image", imageSchema);
+
+export default Image;
