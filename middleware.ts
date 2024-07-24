d7e2f97edb6b7ca9d24c6c8ba090bc06a20436c0
ashish8796 <ashishsaini.ak@gmail.com>
2024-07-24 15:34:08 +0530

    created ui for purchasing credits

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/api/webhooks/clerk",
@@ -9,29 +8,36 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware(
   (auth, req) => {
-    // console.log("Auth: ", auth());
-    if (req.url.includes("/sign-up") && auth().userId) {
-      // console.log("Sign-up Route");
-      // console.log("req.url: ", req.url);
-
-      if (window !== undefined) {
-        window.location.href = "/";
-      }
-
-      return;
-    }
-
-    if (!isPublicRoute(req)) {
-      const { userId } = auth();
-
-      if (!userId) {
-        return auth().redirectToSignIn({ returnBackUrl: req.url });
-      }
-      auth().protect();
-    }
+    if (!isPublicRoute(req)) auth().protect();
   },
   { debug: true }
 );
+
+// export default clerkMiddleware(
+//   (auth, req) => {
+//     // console.log("Auth: ", auth());
+//     if (req.url.includes("/sign-up") && auth().userId) {
+//       // console.log("Sign-up Route");
+//       // console.log("req.url: ", req.url);
+
+//       if (window !== undefined) {
+//         window.location.href = "/";
+//       }
+
+//       return;
+//     }
+
+//     if (!isPublicRoute(req)) {
+//       const { userId } = auth();
+
+//       if (!userId) {
+//         return auth().redirectToSignIn({ returnBackUrl: req.url });
+//       }
+//       auth().protect();
+//     }
+//   },
+//   { debug: true }
+// );
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
